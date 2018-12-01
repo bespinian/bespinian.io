84b31bff8523c4895861dc6077d8daf753cda737
mkretz <mathis.kretz@gmail.com>
2018-12-01 22:59:43 +0100

    initialize vue i18n

--- teambespin/src/main.js
+++ teambespin/src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import "./../node_modules/bulma/css/bulma.css";
 import "./../node_modules/bulma-timeline/dist/css/bulma-timeline.min.css";
 import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   render: h => h(App)
 }).$mount("#app");
