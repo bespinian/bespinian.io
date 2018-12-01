2f7fbf78743c6050af3be9626656bda812fe615c
mkretz <mathis.kretz@gmail.com>
2018-12-02 00:14:47 +0100

    add translations up to and including services section

--- teambespin/vue.config.js
+++ teambespin/vue.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
   }
-}
+};
