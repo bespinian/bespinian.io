464480120f2defc2ebf5d722ede5c54c41fd5736
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2018-12-05 23:48:43 +0100

    Beautify language selector

---
+++ src/components/LocaleChanger.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="locale-changer" class="navbar-item has-dropdown is-hoverable">
+    <a class="navbar-link">{{ langs[$i18n.locale] }}</a>
+    <select class="navbar-dropdown" v-model="$i18n.locale">
+      <option class="navbar-item" v-for="(val, key) in langs" :key="key" :value="key">
+        {{ val }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "locale-changer",
+  data() {
+    return {
+      langs: {
+        en: "English",
+        de: "Deutsch",
+        be: "Bärndütsch"
+      }
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./../node_modules/bulma/css/bulma.css";
 import "./../node_modules/bulma-timeline/dist/css/bulma-timeline.min.css";
 import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import i18n from './i18n'
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
