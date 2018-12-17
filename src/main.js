8605cfdb678b3de2fd097f7a2e492f7b7cba7d33
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2018-12-17 22:29:26 +0100

    Use Lato font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./../node_modules/bulma/css/bulma.css";
 import "./../node_modules/bulma-timeline/dist/css/bulma-timeline.min.css";
+import "typeface-lato";
 import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import i18n from "./i18n";
 import VueScrollTo from "vue-scrollto";
