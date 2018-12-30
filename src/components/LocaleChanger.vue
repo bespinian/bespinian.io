add90daa2b97e3c2b2e623a6189ac6ad826ec7bf
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2018-12-30 13:38:14 +0100

    Fix formatting issues

--- src/components/LocaleChanger.vue
+++ src/components/LocaleChanger.vue
@@ -2,7 +2,12 @@
   <div class="navbar-item has-dropdown is-hoverable">
     <a class="navbar-link">{{ langs[$i18n.locale] }}</a>
     <div class="navbar-dropdown">
-      <a class="navbar-item" v-for="(val, key) in langs" v-on:click="handleLocaleChange(key)" v-bind:key="key" >
+      <a
+        class="navbar-item"
+        v-for="(val, key) in langs"
+        v-on:click="handleLocaleChange(key)"
+        v-bind:key="key"
+      >
         {{ val }}
       </a>
     </div>
@@ -11,7 +16,8 @@
 
 <script>
 import store from "store";
-import {localeStorageKey} from "../i18n";
+import { localeStorageKey } from "../i18n";
+
 export default {
   name: "LocaleChanger",
   data() {
@@ -23,8 +29,8 @@ export default {
       }
     };
   },
-  methods :{
-    handleLocaleChange : function(key) {
+  methods: {
+    handleLocaleChange(key) {
       this.$i18n.locale = key;
       store.set(localeStorageKey, key);
     }
