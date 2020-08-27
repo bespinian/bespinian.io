5329db0700391a7d1beb6abbb81d49bef83d7708
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2020-08-27 23:56:53 +0200

    Upgrade Terraform to 0.13.1

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -26,9 +26,7 @@
         @click="toggleNav()"
       >
         <div class="navbar-end">
-          <router-link to="/" class="navbar-item" href="#">
-            Home
-          </router-link>
+          <router-link to="/" class="navbar-item" href="#">Home</router-link>
           <router-link to="/jobs" class="navbar-item" href="#">
             {{ $t("jobs.nav_item") }}
           </router-link>
@@ -41,9 +39,7 @@
     <section class="section">
       <div class="columns is-centered columns-section">
         <div class="column is-half">
-          <h3 class="title is-2 is-spaced">
-            Cloud Solutions Engineer 60-100%
-          </h3>
+          <h3 class="title is-2 is-spaced">Cloud Solutions Engineer 60-100%</h3>
           <p>
             We are looking to extend our team of consulting software engineers.
             They each advise multiple customers holistically on different
@@ -60,38 +56,22 @@
               You have practical experience with Docker, Kubernetes, OpenShift,
               Cloud Foundry, AWS, Azure and/or GCP
             </li>
-            <li>
-              You are excited about all things DevOps
-            </li>
-            <li>
-              You like to work with multiple customers and technologies
-            </li>
-            <li>
-              You are fluent in English
-            </li>
+            <li>You are excited about all things DevOps</li>
+            <li>You like to work with multiple customers and technologies</li>
+            <li>You are fluent in English</li>
             <li>
               You have worked as a software architect but also as a developing
               engineer
             </li>
-            <li>
-              You live in Switzerland or are willing to relocate
-            </li>
+            <li>You live in Switzerland or are willing to relocate</li>
           </ul>
           <br />
           <h4 class="title is-4">Nice to Have</h4>
           <ul>
-            <li>
-              You are fluent in German and/or French
-            </li>
-            <li>
-              You have experience developing front end applications
-            </li>
-            <li>
-              You have experience developing back end applications
-            </li>
-            <li>
-              You have experience developing microservice architectures
-            </li>
+            <li>You are fluent in German and/or French</li>
+            <li>You have experience developing front end applications</li>
+            <li>You have experience developing back end applications</li>
+            <li>You have experience developing microservice architectures</li>
             <li>
               You have experience with writing automation and infrastructure as
               code
@@ -104,41 +84,25 @@
               You consult our customers on engineering in and around container
               orchestration and the cloud
             </li>
-            <li>
-              You co-create software architectures with customers
-            </li>
+            <li>You co-create software architectures with customers</li>
             <li>
               You help customers implement those architectures in efficient ways
             </li>
-            <li>
-              You train customers' teams in their abilities
-            </li>
-            <li>
-              You help our customers feel at home in cloud environments
-            </li>
+            <li>You train customers' teams in their abilities</li>
+            <li>You help our customers feel at home in cloud environments</li>
           </ul>
           <br />
           <h4 class="title is-4">What We Offer</h4>
           <ul>
-            <li>
-              Flexible ways of working and a lot of responsibility
-            </li>
+            <li>Flexible ways of working and a lot of responsibility</li>
             <li>
               Possibilities of traveling around Switzerland and meeting various
               customers and companies
             </li>
-            <li>
-              A fast paced start up environment
-            </li>
-            <li>
-              A great team of fun people with a similar mindset
-            </li>
-            <li>
-              Employment level: 60-100%
-            </li>
-            <li>
-              Employment date: Flexible
-            </li>
+            <li>A fast paced start up environment</li>
+            <li>A great team of fun people with a similar mindset</li>
+            <li>Employment level: 60-100%</li>
+            <li>Employment date: Flexible</li>
           </ul>
           <br />
           <br />
