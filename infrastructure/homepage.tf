c317d6e0c4762d3af6fab9aab329f66eca821b6a
Lena Fuhrimann <cloudlena@users.noreply.github.com>
2022-02-05 13:58:58 +0100

    Upgrade terraform-spa to 1.0.4

--- infrastructure/homepage.tf
+++ infrastructure/homepage.tf
@@ -1,11 +1,10 @@
 module "homepage" {
-  source = "github.com/cloudlena/terraform-spa?ref=v1.0.3"
+  source = "github.com/cloudlena/terraform-spa?ref=v1.0.4"
   providers = {
     aws.us_east = aws.us_east
   }
 
-  service_name        = "homepage"
-  domain              = local.domain
-  asset_path_patterns = ["js/*", "css/*", "fonts/*", "img/*"]
-  environment         = local.environment
+  service_name = "homepage"
+  domain       = local.domain
+  environment  = local.environment
 }
