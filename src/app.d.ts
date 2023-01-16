b4f13e05af7930105f4bd2d3261f700f91298f65
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-01-16 11:01:22 +0100

    Update dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,19 +1,19 @@
-declare module 'svelte-carousel';
-declare module '@beyonk/svelte-google-analytics';
-declare module '@beyonk/gdpr-cookie-consent-banner';
-
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Error {}
-	// interface Locals {}
-	interface PageData {
-		customer?: {
-			en: import('$lib/customers/Customer').Customer;
-			de: import('$lib/customers/Customer').Customer;
-			logo: string;
-		};
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		interface PageData {
+			customer?: {
+				en: import('$lib/customers/Customer').Customer;
+				de: import('$lib/customers/Customer').Customer;
+				logo: string;
+			};
+		}
+		// interface Platform {}
 	}
-	// interface Platform {}
 }
+
+export {};
