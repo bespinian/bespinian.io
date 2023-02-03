a7326cac448cf6bc19c730e5c7a3004b55f5d22c
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-02-03 15:15:32 +0100

    Add www sub domain

--- infrastructure/homepage.tf
+++ infrastructure/homepage.tf
@@ -1,10 +1,11 @@
 module "homepage" {
-  source = "github.com/cloudlena/terraform-spa?ref=v1.0.6"
+  source = "github.com/cloudlena/terraform-spa?ref=v1.1.0"
   providers = {
     aws.us_east = aws.us_east
   }
 
-  service_name = "homepage"
-  domain       = local.domain
-  environment  = local.environment
+  service_name         = "homepage"
+  domain               = local.domain
+  alternate_subdomains = ["www"]
+  environment          = local.environment
 }
