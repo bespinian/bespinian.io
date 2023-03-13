d13ba97d156c5508bc8da497f06bf16ac899e2bc
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-03-13 09:47:57 +0100

    Add SPOUD to partners

--- src/routes/Partners.svelte
+++ src/routes/Partners.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
-	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-	import '@splidejs/svelte-splide/css';
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
 	import acend from '$lib/images/acend.webp';
 	import swisscom from '$lib/images/swisscom.webp';
 	import vshn from '$lib/images/vshn.svg';
 	import aws from '$lib/images/aws.webp';
 	import puzzle from '$lib/images/puzzle.webp';
+	import spoud from '$lib/images/spoud.svg';
 	import redHat from '$lib/images/redhat.webp';
 	import swissMadeSoftware from '$lib/images/swissmadesoftware.webp';
 	import peakscale from '$lib/images/peakscale.svg';
@@ -25,6 +26,7 @@
 		{ name: 'VSHN', logo: vshn, link: 'https://vshn.ch' },
 		{ name: 'acend', logo: acend, link: 'https://acend.ch' },
 		{ name: 'Swisscom', logo: swisscom, link: 'https://www.swisscom.com' },
+		{ name: 'SPOUD', logo: spoud, link: 'https://www.spoud.io' },
 		{ name: 'AWS Partner Network', logo: aws, link: 'https://aws.amazon.com/partners/' },
 		{ name: 'Red Hat', logo: redHat, link: 'https://redhat.com' },
 		{ name: 'Swiss Made Software', logo: swissMadeSoftware, link: 'https://swissmadesoftware.org' }
@@ -38,13 +40,21 @@
 			<p>{$_('partners.body')}:</p>
 			<br />
 			<div class="container">
-				<Splide options={{ rewind: true, perPage: 3 }} aria-label="ASDa">
-					{#each partners as partner (partner.name)}
-						<SplideSlide>
-							<img src={partner.logo} alt={partner.name} width="120" height="120" />
-						</SplideSlide>
-					{/each}
-				</Splide>
+				{#if browser}
+					<Carousel particlesToShow={3} autoplay={true} dots={false}>
+						{#each partners as partner (partner.name)}
+							<div class="has-text-centered">
+								<div class="has-image-centered customer-logo">
+									<figure class="image">
+										<img src={partner.logo} alt={partner.name} width="120" height="120" />
+									</figure>
+									<br />
+								</div>
+								<a href={partner.link} target="_blank" rel="noopener noreferrer">{partner.name}</a>
+							</div>
+						{/each}
+					</Carousel>
+				{/if}
 			</div>
 		</div>
 	</div>
