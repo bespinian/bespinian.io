ee89e7d2bafe7d9ec828a245fe3f91a43c26268f
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-08-30 22:08:16 +0200

    Update dependencies

--- infrastructure/homepage.tf
+++ infrastructure/homepage.tf
@@ -1,5 +1,5 @@
 module "homepage" {
-  source = "github.com/cloudlena/terraform-aws-spa?ref=v2.0.1"
+  source = "github.com/cloudlena/terraform-aws-spa?ref=v2.0.2"
   providers = {
     aws.us_east = aws.us_east
   }
