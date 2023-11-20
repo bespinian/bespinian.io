9c28ea1fcc1f2b1413b34708d66c42eb07f0622b
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-11-20 10:53:09 +0100

    Add imprint

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
+
 	const year = new Date().getFullYear();
 </script>
 
 <footer class="footer">
-	<div class="content has-text-centered">
-		<p><strong>bespinian</strong> {year}</p>
+	<div class="content">
+		<div class="columns">
+			<div class="column"></div>
+			<div class="column has-text-centered">
+				<p><strong>bespinian</strong> {year}</p>
+			</div>
+			<div class="column has-text-right">
+				<a href="/imprint" class="is-right">{$_('imprint.title')}</a>
+			</div>
+		</div>
 	</div>
 </footer>
